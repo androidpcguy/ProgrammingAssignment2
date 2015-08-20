fde6916ec9afa92c152d3338adbd8325ce7fceab
Akshara Balachandra <akshara.bala.18@gmail.com>
2015-08-19 20:20:38 -0700

    Completed Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the following two function is to cache the computed inverse 
+## of some matrix 'x' so that we do not have to repeatedly compute it. This 
+## allows us to save computational resources.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	invMatrix <- NULL
+	setmatrix <- function(y) {
+		x <<- y
+		invMatrix <<- NULL
+	}
 
+	getmatrix <- function() x
+	setinvmatrix <- function(inverse) invMatrix <<- inverse
+	getinvmatrix <- function() invMatrix
+	list(	setmatrix = setmatrix,
+		getmatrix = getmatrix,
+		setinvmatrix = setinvmatrix,
+		getinvmatrix = getinvmatrix
+	);
 }
 
 
@@ -12,4 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	invmatrix <- x$getinvmatrix()
+	if(is.null(invmatrix)) {
+		m <- x$get()
+		invmatrix <- solve(m)
+		x$setinvmatrix(invmatrix)
+		return(invmatrix)
+	}
+	x$getinvmatrix()
 }
